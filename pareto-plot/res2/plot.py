21b0a3d2d7b3cb6cc674b2c04dc5a90fdb610c77
Morten Gjerding <mortengjerding@gmail.com>
2020-11-10 22:23:39 +0100

    Make function that can combine perspectives on 2D atomic structure

--- pareto-plot/res2/plot.py
+++ pareto-plot/res2/plot.py
@@ -6,6 +6,8 @@ from matplotlib import pyplot as plt
 from ase.db import connect
 from ase import Atoms
 from pathlib import Path
+from ase.io.pov import get_bondpairs
+import PIL
 
 rmsdresult = read_json('results-asr.database.rmsd.json')
 rmsd_by_id = rmsdresult['rmsd_by_id']
@@ -44,19 +46,21 @@ rows = {row.uid: row for row in db.select()}
 natoms_in_image = 30
 
 
-def cut_out_square_sheet(atoms):
+def cut_square_sheet(atoms):
     """Cut out a square sheet of 2D material."""
+    delta = 0.001
     atoms = atoms.repeat((10, 10, 1))
     atoms.center()
     atoms.wrap()
     cutsize = 20
     cut_cv = np.diag([cutsize, ] * 3)
-    pos_av = atoms.positions - atoms.positions[0] + [0, 0, cutsize / 2]
+    pos_av = atoms.positions - atoms.positions[0] + [delta, delta, cutsize / 2]
     spos_cut_ac = np.dot(pos_av, np.linalg.inv(cut_cv))
     mask = np.logical_and(spos_cut_ac < 1.0,
                           spos_cut_ac > 0.0).all(axis=1)
-    new_atoms = Atoms([atom for atom, thismask
-                       in zip(atoms, mask) if thismask])
+    new_atoms = Atoms([atom.symbol for atom, thismask
+                       in zip(atoms, mask) if thismask],
+                      positions=pos_av[mask, :], pbc=False)
     return new_atoms
 
 
@@ -75,12 +79,17 @@ def run_povray(
         display=False,
         canvas_width=1000,
         rotation='-90x,-30y',
+        radii=0.5,
+        bondatoms=None,
         **kwargs,
 ):
     """Run povray and save to file.
 
     Filename defaults to {formula}.png.
     """
+    if bondatoms is None:
+        bondatoms = get_bondpairs(new_atoms, radius=0.75)
+
     if filename is None:
         filename = f'{atoms.symbols.formula:metal}.pov'
     print(f'Povray running for {filename}')
@@ -92,14 +101,17 @@ def run_povray(
         display=display,
         rotation=rotation,
         canvas_width=canvas_width,
+        radii=radii,
+        bondatoms=bondatoms,
         **kwargs,
     )
     _clean_povray_files(filename)
+    return str(Path(filename).with_suffix('.png'))
 
 
-def make_images_from_multiple_rotations(
+def make_images_from_multiple_perspectives(
         atoms,
-        rotations=['0x,0y', '-90x,-30y', '-30x,-90y'],
+        rotations=['0x,0y', '-90x', '90y'],
         basename=None,
         **kwargs,
 ):
@@ -110,23 +122,61 @@ def make_images_from_multiple_rotations(
     atoms: Atoms
         Atomic structure to make figure of.
     rotations: List[str]
-        Povray camera rotations.
+        Povray object rotations. '90x'=rotate 90deg around x axis.
+        '90x,90y'=rotate 90deg around x axis and then 90 deg around y axis.
     basename: str
         Base for filename, for example "MoS2" would give files like
         MoS2-'0x,0y'.png. Defaults to {atoms.symbols.formula:metal}
     kwargs: dict
         Key word arguments handed through to ase-povray interface.
 
+    Returns
+    -------
+    filenames: List[str]
+        List of path of created files.
+
     """
     if basename is None:
         basename = format(atoms.symbols.formula, 'metal')
+    filenames = []
     for rotation in rotations:
         rotation_string = rotation.replace(',', '')
-        run_povray(
+        filename = run_povray(
             new_atoms,
             filename=f'{basename}-{rotation_string}.pov',
             rotation=rotation,
             **kwargs)
+        filenames.append(filename)
+    return filenames
+
+
+def make_image_of_2D_material_from_multiple_perspectives(atoms,
+                                                         filename=None):
+
+    if filename is None:
+        formula = atoms.symbols.formula
+        filename = f'{formula:metal}_perspectives.png'
+    new_atoms = cut_square_sheet(atoms)
+    filenames = make_images_from_multiple_perspectives(
+        new_atoms,
+        rotations=['0x,0y', '-90x', '90y'],
+        basename=format(atoms.symbols.formula,
+                        'metal'),
+    )
+
+    images = []
+    for filename in filenames:
+        images.append(PIL.Image.open(filename))
+
+    widths, heights = zip(*(i.size for i in images))
+    total_width = widths[0] + widths[2]
+    total_height = heights[0] + heights[1]
+    new_image = PIL.Image.new('RGBA', (total_width, total_height))
+    new_image.paste(images[0], box=(images[2].size[0], 0))
+    new_image.paste(images[1], box=(images[2].size[0], images[1].size[1]))
+    new_image.paste(images[2], box=(0, 0))
+    new_image.save(filename)
+
 
 
 max_cluster_id = max(clusters)
@@ -143,9 +193,10 @@ for treated_cluster_id in [1]:  # range(max_cluster_id):
         pareto_data_x.append(row.natoms)
         pareto_data_y.append(row.hform)
         atoms = row.toatoms()
-        new_atoms = cut_out_square_sheet(atoms)
-        make_images_from_multiple_rotations(
-            new_atoms,
-            basename=format(atoms.symbols.formula,
-                            'metal')
-        )
+        new_atoms = cut_square_sheet(atoms)
+        make_image_of_2D_material_from_multiple_perspectives(atoms)
+        # make_images_from_multiple_perspectives(
+        #     new_atoms,
+        #     basename=format(atoms.symbols.formula,
+        #                     'metal'),
+        # )
