601011c6cf4ff7d6d7c689205516748fa1226c9c
Morten Gjerding <mogje@fysik.dtu.dk>
2020-11-17 11:01:54 +0100

    Update plotting script

--- pareto-plot/res2/plot.py
+++ pareto-plot/res2/plot.py
@@ -16,8 +16,24 @@ from utils import (
     cut_square_sheet,
     make_image_of_2D_material_from_multiple_perspectives
 )
+from rcparams import plotter, textwidth, columnwidth
 
 
+def make_pareto_lines(x_p, y_p):
+    points_pv = sorted(zip(x_p, y_p), key=lambda x: x[0])
+    x_p = [point_v[0] for point_v in points_pv]
+    y_p = [point_v[1] for point_v in points_pv]
+    paddedx_p = x_p + [20]
+    paddedy_p = [0] + y_p
+
+    for x1, x2, y in zip(paddedx_p[:-1], paddedx_p[1:], y_p):
+        plt.plot([x1, x2], [y, y], color='k')
+
+    for y1, y2, x in zip(paddedy_p[:-1], paddedy_p[1:], x_p):
+        plt.plot([x, x], [y1, y2], color='k')
+
+
+@plotter()
 def plot():
     """Plot atomic structures for RMSD figure."""
     rmsdresult = read_json('results-asr.database.rmsd.json')
@@ -37,8 +53,8 @@ def plot():
     condensed_distance_matrix = squareform(distance_matrix)
     linkage_matrix = linkage(condensed_distance_matrix, optimal_ordering=False)
 
-    plt.figure()
-    dendrogram(linkage_matrix, labels=uids)
+    # plt.figure()
+    # dendrogram(linkage_matrix, labels=uids)
     clusters = fcluster(linkage_matrix, 0.5, criterion='distance')
 
     db = connect('res2.db')
@@ -56,13 +72,20 @@ def plot():
         pareto_data_x.append(row.natoms)
         pareto_data_y.append(row.hform)
         atoms = row.toatoms()
-        make_image_of_2D_material_from_multiple_perspectives(atoms)
+        # make_image_of_2D_material_from_multiple_perspectives(atoms)
 
-    plt.figure()
+    plt.figure(figsize=(textwidth, columnwidth))
     plt.scatter(pareto_data_x, pareto_data_y)
-    print('Pareto data X=', pareto_data_x)
-    print('Pareto data Y=', pareto_data_y)
-    # plt.show()
+    plt.xlabel('Number of atoms')
+    plt.ylabel('HOF [eV/atom]')
+    plt.xlim(0, 13)
+    ylim = plt.ylim()
+    plt.xticks(range(0, 14))
+    make_pareto_lines(pareto_data_x, pareto_data_y)
+    plt.ylim(ylim)
+    plt.tight_layout()
+    plt.savefig('pareto.png', dpi=300)
+    plt.show()
 
 
 if __name__ == '__main__':
