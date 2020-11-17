e8f10c1652df55edcdb99e171b5491e04e668abf
fBert31 <fafb@dtu.dk>
2020-11-17 15:08:48 +0100

    dimensionality figure in columnsize

--- dimensionality-plot/plot.duplicates-figure.py
+++ dimensionality-plot/plot.duplicates-figure.py
@@ -1,27 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import matplotlib.image as mpimg
 import os
 import sys
-from ase.io.pov import get_bondpairs
-
 p = os.path.abspath('/home/niflheim/fafb/papers/c2db2/c2db-version-2/')
+
 if p not in sys.path:
     sys.path.append(p)
 
-from utils import cut_square_sheet, make_image_of_2D_material_from_multiple_perspectives, run_povray
-from ase.io import read
+from rcparams import rcParams, columnwidth
 
-structures = ['initial_C2', 'initial_Ge2Se2', 'final_C2', 'final_Ge2Se2']
-for name in structures:
-    atoms = read(name + '.json')
-    atoms = cut_square_sheet(atoms)
-    if name.split('_')[-1] == 'C2':
-        bondatoms = None
-    elif name.split('_')[-1] == 'Ge2Se2':
-        bondatoms = get_bondpairs(atoms, radius=1.)
-    # top view
-    filename = run_povray(atoms, rotation='0x,0y', bondatoms=bondatoms)
-    # rename file
-    os.system(f'mv {filename} {name}_top.png')
-    # cut view
-    filename = run_povray(atoms, bondatoms=bondatoms)
-    # rename file
-    os.system(f'mv {filename} {name}_cut.png')
+img = mpimg.imread('dimensionality_structures.png')
+
+fig, ax = plt.subplots()
+figsize = (columnwidth, columnwidth*75/79.502)
+fig.set_size_inches(*figsize)
+
+im = ax.imshow(img)
+
+plt.text(750, 180, '$N_{2D}^{initial} = 1$')
+plt.text(750, 280, '$N_{2D}^{final} = 1$')
+plt.text(750, 600, '$N_{2D}^{initial} = 1$')
+plt.text(750, 700, '$N_{2D}^{final} = 2$')
+
+plt.axis('off')
+plt.savefig('dimensionality.pdf')
