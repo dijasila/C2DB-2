d79bab5b9aa5aeecae04be5234c647f53f4560a5
Morten Gjerding <mortengjerding@gmail.com>
2021-01-13 14:43:06 +0100

    Update recipes table with a count column

--- recipe-params-table/table.py
+++ recipe-params-table/table.py
@@ -1,3 +1,5 @@
+import pathlib
+
 text = r"""
 Magnetic anisotropies  & PBE(*) & Magnetic \\
 Exchange couplings & PBE & Magnetic \\
@@ -7,12 +9,12 @@ Heat of formation & PBE & \\
 Energy above convex hull & PBE & \\
 Out-of-plane dipole & PBE & \\
 Work function & PBE(*) & $E_{\mathrm{gap}}=0$ \\
-\textcolor{red}{Fermi arc} & PBE(*) & $E_{\mathrm{gap}}=0$ \\
+Fermi surface & PBE(*) & $E_{\mathrm{gap}}=0$ \\
 Deformation potentials & PBE(*) & $E_{\mathrm{gap}}>0$ \\
 Bader charges & PBE & \\
-Electronic band structure  & PBE(*) &   \\
-Electronic band structure  & HSE06@PBE(*)  &   \\
-Electronic band structure  & G$_0$W$_0$@PBE(*) & $E_{\mathrm{gap}}>0$, $N_{\mathrm{atoms}}<5$ \\
+Electronic band structure PBE & PBE(*) &   \\
+Electronic band structure HSE & HSE06@PBE(*)  &   \\
+Electronic band structure GW & G$_0$W$_0$@PBE(*) & $E_{\mathrm{gap}}>0$, $N_{\mathrm{atoms}}<5$ \\
 Orbital projected band structure & PBE & \\
 Projected density of states & PBE & \\
 Effective masses & PBE(*) &  $E_{\mathrm{gap}}>0$ \\
@@ -26,15 +28,72 @@ Infrared polarisability & PBE & $E_{\mathrm{gap}}>0$  \\
 Optical absorbance  &  BSE@PBE-scissors(*) &  $E_{\mathrm{gap}}>0$,  $N_{\mathrm{atoms}}<5$  \\
 Raman spectrum & PBE + DZP basis set & NM, dyn. stable, $k_d=25$ {\AA}$^{-1}$ \\
 Second harmonic generation & PBE & $E_{\mathrm{gap}}>10 meV$, NM, non-centrosym., $k_d=30$ {\AA}$^{-1}$"""
+
+
+column_to_name = {
+    'Magnetic anisotropies': 'magnetic_anisotropy',
+    'Exchange couplings': 'exchange',
+    r'Phonons ($\Gamma$ and BZ corners)': 'phonons',
+    'Stiffness tensor': 'stiffness',
+    'Heat of formation': 'convex_hull',
+    'Energy above convex hull': 'convex_hull',
+    'Out-of-plane dipole': 'gs',
+    'Work function': 'gs',
+    'Fermi surface': 'fermisurface',
+    'Deformation potentials': 'deformationpotentials',
+    'Bader charges': 'bader',
+    'Electronic band structure PBE': 'bandstructure',
+    'Electronic band structure HSE': 'hse',
+    'Electronic band structure GW': 'gw',
+    'Orbital projected band structure': 'projected_bandstructure',
+    'Projected density of states': 'pdos',
+    'Effective masses': 'emasses',
+    'Plasma frequency': 'plasmafrequency',
+    'Topological invariants': 'berry',
+    'Born charges': 'borncharges',
+    'Spontaneous polarisation': 'ferroelectricity',
+    'Piezoelectric tensor': 'piezoelectrictensor',
+    'Optical polarisability': 'polarizability',
+    'Infrared polarisability': 'infraredpolarizability',
+    'Optical absorbance': 'bse',
+    'Raman spectrum': 'raman',
+    'Second harmonic generation': 'shg',
+}
+
+count_kvp = pathlib.Path('count_kvp.txt').read_text()
+
+name_to_count = {}
+for line in count_kvp.split('\n'):
+    line = line.replace(' ', '')
+    if not line.startswith('has_asr'):
+        continue
+    name, count = line.split(':')
+    count = int(count)
+    name = name[8:]
+    name_to_count[name] = count
+
+name_to_count.update(
+    {
+        'ferroelectricity': 'XXX No count yet',
+        'shg': 'XXX No count yet',
+    }
+)
+
 lines = text.split(r'\\')
 
 rows = []
 for line in lines:
     columns = line.split('&')
     columns = [column.strip() for column in columns]
+    # Append number of materials calculated
+    name = column_to_name[columns[0]]
+    count = name_to_count[name]
+    columns.append(str(count))
     rows.append(columns)
+    # print("'" + columns[0] + "': '',")
 
 rows = sorted(rows, key=lambda x: (len(x[1]), len(x[2]), x[0]))
+
 
 for row in rows:
     print(' ' * 8 + ' & '.join(row) + r' \\')
